4ab4d00255cf864a5580ded97c4cd24e62028e57
Lekarri <lekarrikaila567@gmail.com>
2022-09-08 10:47:08 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main -  prints the phrase programming is like building a multilingual puzzle
+ *
+ * Return: 0 if exited well, nonzero if otherwise
+ */
+int main(void)
+{
+	puts("programming is like building a multilingual puzzle\n");
+	return(0);
+
+}
