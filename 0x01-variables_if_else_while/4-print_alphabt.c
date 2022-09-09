f98083425043dee1e740e4996c3795be47d2774a
Lekarri <lekarrikaila567@gmail.com>
2022-09-09 12:45:19 -0700

    alphabt

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,17 +6,16 @@
 int main(void)
 {
 	char ch = 'a';
-	
 	while (ch <= 'z')
 	{
-		if (ch!='e' && ch!='q')
+		if (ch !='e' && ch !='q')
 		{
 			putchar (ch);
 		}
 		ch++;
-		}
-	        putchar('\n');
-		return (0);
+	}
+	putchar('\n');
+	return (0);
 }
 
 
