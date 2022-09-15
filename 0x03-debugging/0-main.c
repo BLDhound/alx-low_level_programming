7b624fc2f100e47b8edde6f2f8a3ca146e5c919f
Lekarri <lekarrikaila567@gmail.com>
2022-09-15 08:55:56 -0700

    debug test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,1 @@
-#include "main.h"
-/**
- * postitive_or_negative() - tests function that prints,
- * if integer is positive or negative
- * @n: integer to be tested
- * Return: 0
- */
-int postitive_or_negative()
-{
-	int n;
 
-	n = 0;
-	positive_or_negative(n);
-
-	return (0);
-}
