742b6bdb24ad8ff8d9e6b866784f1a8843f4b76a
Lekarri <lekarrikaila567@gmail.com>
2022-09-15 02:47:18 -0700

    update debug

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "stdlib.h"
+#include "time.h"
+#include "stdio.h"
 /**
  * postitive_or_negative() - tests function that prints,
  * if integer is positive or negative
