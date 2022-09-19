5cb77fe8374c553fa85e23d1728a79b81e1458bc
Lekarri <lekarrikaila567@gmail.com>
2022-09-19 11:10:18 -0700

    string length

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strelen - function that returns the length of a string
+ * @s: parameter used
+ * Return: integer
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *(s + n) != '\0'; n++)
+	{}
+	return (n);
+}
