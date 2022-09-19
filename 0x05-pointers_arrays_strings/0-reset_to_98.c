33805bd44db53b48e4e35b3ce6617868eca2e9de
Lekarri <lekarrikaila567@gmail.com>
2022-09-19 10:51:46 -0700

    updated 0.98 description

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * reset_to_98 - resets *n to 98
- * @n - parameter used in the function
+ * *n - parameter used in the function
  * Return: Always 0
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
