071952a4375b9d8a6da1f823cfb815cb13970e93
Lekarri <lekarrikaila567@gmail.com>
2022-10-13 11:03:58 -0700

    operations

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,29 @@
+#include "3-calc.h"
+/**
+ * get_op_func - function that selects correct function to perform an op
+ * @s: first parameter
+ * Return: pointer to correct operation function
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	int i;
+
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+
+	while (i < 5)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+	printf("Error\n"), exit(99);
+}
