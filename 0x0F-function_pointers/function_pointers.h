ad04a94b08b62b899d64ebdc189ef925ca837d26
Lekarri <lekarrikaila567@gmail.com>
2022-10-13 10:04:23 -0700

    print name

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,10 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-typedef _SIZE_TYPE_ size_t;
+typedef __SIZE_TYPE__ size_t;
 
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
-int int_index(int *array, int size, int (*cmp)(int));
 
 #endif
