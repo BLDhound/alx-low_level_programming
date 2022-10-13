1bba7e4c7c75592191a447b25d9800cf1b17a417
Lekarri <lekarrikaila567@gmail.com>
2022-10-13 11:13:17 -0700

    operations and functions

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,51 @@
+#include "3-calc.h"
+/**
+ * op_add - function that adds 
+ * @a: integer
+ * @b: second integer
+ * Return: int
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - function that subtracts operations
+ * @a: first integer parameter
+ * @b: second integer parameter
+ * Return: int
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - function that multiplies
+ * @a: first parameter
+ * @b: second parameter
+ * Return: int
+ */
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+/**
+ * op_div - function to divide
+ * @a: first parameter
+ * @b: second parameter
+ * Return: int
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - a modulous function, to get remainder
+ * @a: first parameter
+ * @b: second parameter
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
