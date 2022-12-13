a718361fe4c682e2f7e3aa70fafef2518fbf89aa
Lekarri <lekarrikaila567@gmail.com>
2022-12-13 00:03:34 -0800

    my trials

---
+++ 0x18-dynamic_libraries/_isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isdigit - checks if c is a digit
+ * @c: number in ASCII 
+ * Return: 1 if true 0 if otherwise
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	return (0);
+}
